592bf8967f4d5a034c3252d205514d1257b2ead6
iwishiwasaneagle <jh.ewers@gmail.com>
2021-01-03 16:23:49 +0000

    Setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pypolytraj-iwishiwasaneagle",
+    version="0.1",
+    author="Jan-Hendrik Ewers",
+    author_email="jh.ewers@gmail.com",
+    description="Polynomial Trajectory Generation",
+    long_description="A package for Polynomial Trajectory Generation (quintic, multi-bounded, etc.)",
+    long_description_content_type="text/markdown",
+    url="https://github.com/iwishiwasaneagle/pytrajgen",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
